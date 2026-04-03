693d9f3178bc1a2696f9893c3c735ba12ee2df5c
Guilherme Tófoli <guitofoli1806@gmail.com>
2026-04-02 21:15:34 -0300

    Criado colunas da tabela para armazenar os dados do usuário

--- app/src/main/java/com/tofoli/userregistrationapp/User.java
+++ app/src/main/java/com/tofoli/userregistrationapp/User.java
@@ -1,4 +1,17 @@
 package com.tofoli.userregistrationapp;
 
+// Importa anotações do Room necessários para mapear esta classe como uma entidade no banco de dados//
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
 public class User {
+
+    //Define o campo 'id' como chave primária da tabela e conifugra pra ser gerado automaticamente
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+
+    //Campos que representam as colunas da tabela para armazenar os dados do usuário//
+    private String name; //Nome do Usuário
+    private String cpf; //CPF do Usuário
+    private String email; //Email do Usuário
+    private String phone; //Fone do Usuário
 }
