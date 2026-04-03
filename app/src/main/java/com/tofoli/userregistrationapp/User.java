d91ec88278ae0d3a831a51cb45544ac4e9004d91
Guilherme Tófoli <guitofoli1806@gmail.com>
2026-04-02 21:27:58 -0300

    Iniciado o método GET e SETTER

--- app/src/main/java/com/tofoli/userregistrationapp/User.java
+++ app/src/main/java/com/tofoli/userregistrationapp/User.java
@@ -3,6 +3,9 @@ package com.tofoli.userregistrationapp;
 // Importa anotações do Room necessários para mapear esta classe como uma entidade no banco de dados//
 import androidx.room.Entity;
 import androidx.room.PrimaryKey;
+
+import java.util.Locale;
+
 public class User {
 
     //Define o campo 'id' como chave primária da tabela e conifugra pra ser gerado automaticamente
@@ -14,4 +17,34 @@ public class User {
     private String cpf; //CPF do Usuário
     private String email; //Email do Usuário
     private String phone; //Fone do Usuário
+
+    //Construtor da classe que será usado para criar novo objeto
+
+    public User(String name, String cpf,String email,String phone)
+    {
+        this.name = name;
+        this.cpf = cpf;
+        this.email = email;
+        this.phone = phone;
+    }
+
+    //Métodos getter e setter para acessar e modificar os dados do objeto User
+
+    //Retorna o ID do usuário
+    public int getId(){return id;}
+
+    //Define o ID do usuário (usado pelo Room para preencher automaticamente)
+    public void setid(int id) {this.id = id;}
+
+    //Retorna o nome do usuário
+    public String getName(){return name;}
+
+    //Retorna CPF do usuário
+    public String getCpf(){return cpf;}
+
+    //Retorna o email do usuário
+    public String getEmail(){return email;}
+
+    //Retorna o phone do usuário
+    public String getPhone(){return phone;}
 }
