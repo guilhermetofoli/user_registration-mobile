3a50546b2b66df24b3c12353a8539014259fc1a2
Guilherme Tófoli <guitofoli1806@gmail.com>
2026-04-09 19:59:40 -0300

    Corrigido escrita e adicionado função para navegar entre as telas

--- app/src/main/java/com/tofoli/userregistrationapp/ReportActivity.java
+++ app/src/main/java/com/tofoli/userregistrationapp/ReportActivity.java
@@ -1,4 +1,35 @@
 package com.tofoli.userregistrationapp;
 
-public class ReportActivity {
+//Importações de componentes de UI, Intenções e biblioteca room
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ReportActivity extends AppCompatActivity {
+    //Campo de texto onde os dados do banco serão exibidos
+    private TextView textViewReport;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        //Define o layout XML dessa tela de relatório
+        setContentView(R.layout.activity_report);
+        //Mapeamento do TextView do XML para o java
+        textViewReport = findViewById(R.id.textViewReport);
+        //Encontra o botão e define o clique para voltar
+        Button btnVoltar = findViewById(R.id.btnVoltar);
+        //O botão de retorno utilizando expressão lambda
+        btnVoltar.setOnClickListener(v -> voltarParaCadastro());
+    }
+
+    // Método resposnável pela navegação entre as telas do app
+    public void voltarParaCadastro(){
+        //Intenção para abrir a tela de cadastro
+        Intent intent = new Intent(ReportActivity.this, MainActivity.class);
+        startActivity(intent);
+        //Fecha a tela de relatório para não acumular na pilha de tarefas
+        finish();
+    }
 }
