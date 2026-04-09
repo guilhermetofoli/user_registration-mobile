1f6e53185e0c94bc79cb9e412eff43136206977a
Guilherme Tófoli <guitofoli1806@gmail.com>
2026-04-09 20:11:23 -0300

    Iniciado a conexão com o Banco de Dados

--- app/src/main/java/com/tofoli/userregistrationapp/ReportActivity.java
+++ app/src/main/java/com/tofoli/userregistrationapp/ReportActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.widget.Button;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.room.Room;
 
 public class ReportActivity extends AppCompatActivity {
     //Campo de texto onde os dados do banco serão exibidos
@@ -22,7 +23,18 @@ public class ReportActivity extends AppCompatActivity {
         Button btnVoltar = findViewById(R.id.btnVoltar);
         //O botão de retorno utilizando expressão lambda
         btnVoltar.setOnClickListener(v -> voltarParaCadastro());
+
+        /*
+        Conexão com o banco de dados
+        1 - Cria uma instância do Banco "user-database"
+        2 - .allowMainThreadQueries(): Serve para liberar operações de consulta feitas em threads da UI.
+        Por padrão, ROOM proibe isso. o correto seria fazer consultas em threads separadas;
+         */
+
+        UserDatabase db = Room.databaseBuilder(getApplication(),UserDatabase.class, "user-database"). allowMainThreadQueries().build();
+
     }
+
 
     // Método resposnável pela navegação entre as telas do app
     public void voltarParaCadastro(){
