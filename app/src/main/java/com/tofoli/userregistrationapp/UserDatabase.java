18a7ae1a310a7335fef8b03d4dbbf931a2954afa
Guilherme Tófoli <guitofoli1806@gmail.com>
2026-04-09 21:32:14 -0300

    Retorno da instância do BD

--- app/src/main/java/com/tofoli/userregistrationapp/UserDatabase.java
+++ app/src/main/java/com/tofoli/userregistrationapp/UserDatabase.java
@@ -1,5 +1,6 @@
 package com.tofoli.userregistrationapp;
 //Importa as anotações e classes da bibilioteca Room e do Android
+import android.content.Context; //Classe que representa o contexto da aplicação (Necessário para acessar recursos)
 import androidx.room.Database; // Anotação para marcar a classe como um banco de dados Room
 import androidx.room.RoomDatabase; //Classe base que representa o BD
 
@@ -17,4 +18,10 @@ public abstract class UserDatabase extends RoomDatabase {
     //Método abstrato que será implementado pela Room
     //Serve para acessar as operações do BD definidas na UserDao
     public abstract UserDao userDao();
+
+    //Método que retorna a instância do BD
+    //O uso do 'syncronized' garante que apenas uma thread possa acessar este método por vez, evitando que duas instâncias do BD sejam criadas acidentalmente
+    public static synchronized UserDatabase getInstance(Context context){
+
+    }
 }
