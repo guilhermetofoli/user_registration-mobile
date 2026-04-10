189a2abf85b24b56c315e502de50172f2492d6f0
Guilherme Tófoli <guitofoli1806@gmail.com>
2026-04-09 21:25:58 -0300

    Acessar operações do BD na UserDAO

--- app/src/main/java/com/tofoli/userregistrationapp/UserDatabase.java
+++ app/src/main/java/com/tofoli/userregistrationapp/UserDatabase.java
@@ -1,4 +1,20 @@
 package com.tofoli.userregistrationapp;
+//Importa as anotações e classes da bibilioteca Room e do Android
+import androidx.room.Database; // Anotação para marcar a classe como um banco de dados Room
+import androidx.room.RoomDatabase; //Classe base que representa o BD
 
-public class UserDatabase {
+
+/*
+Anotação @Database define que a classe representa o banco de dados Room
+Ela específica as entidades (tabelas) que o banco irá conter e a versão do BD
+ */
+
+@Database(entities = {User.class}, version = 1)
+public abstract class UserDatabase extends RoomDatabase {
+    //Instância única (singleton) do BD
+    private static UserDatabase Instance;
+
+    //Método abstrato que será implementado pela Room
+    //Serve para acessar as operações do BD definidas na UserDao
+    public abstract UserDao userDao();
 }
